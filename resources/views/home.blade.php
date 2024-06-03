cdbf4b6c8c068443e6b0b712f2cec7e0a08ca06c
jahanzab sabir <jjahanzab226@gmail.com>
2024-06-03 14:41:54 +0500

    view changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Welcom to Larave AWS App!') }}
                 </div>
             </div>
         </div>
